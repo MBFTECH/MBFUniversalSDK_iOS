62a549f7ecec4b701d1d89ecb73248294ee4fe40
yen phan <phanyenngoc11@gmail.com>
2023-10-25 13:54:01 +0700

    Add SPM

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MBFUniversalSDK",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MBFUniversalSDK",
+            targets: ["MBFUniversalSDK"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MBFUniversalSDK",
+            path: "./0.1.15/MBFUniversalSDK.xcframework.zip")
+    ]
+)
